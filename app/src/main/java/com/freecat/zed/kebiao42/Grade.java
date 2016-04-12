62c1955dab74fc101f2c1cad11a86be760d5ccd8
ZfreeCat <421677460@qq.com>
2016-04-12 17:44:18 +0800

    third Change

---
+++ app/src/main/java/com/freecat/zed/kebiao42/Grade.java
@@ -0,0 +1,98 @@
+package com.freecat.zed.kebiao42;
+
+/**
+ * Created by zed on 4/5/16.
+ */
+public class Grade {
+    String g_y;
+    String g_t;
+    String g_cn;
+    String g_p;
+    String g_ct;
+    String g_cg;
+    String g_gg;
+    String g_fg;
+    String g_ng;
+    String g_mg;
+
+    public String getG_y() {
+        return g_y;
+    }
+
+    public void setG_y(String g_y) {
+        this.g_y = g_y;
+    }
+
+    public String getG_cg() {
+        return g_cg;
+    }
+
+    public void setG_cg(String g_cg) {
+        this.g_cg = g_cg;
+    }
+
+    public String getG_cn() {
+        return g_cn;
+    }
+
+    public void setG_cn(String g_cn) {
+        this.g_cn = g_cn;
+    }
+
+    public String getG_ct() {
+        return g_ct;
+    }
+
+    public void setG_ct(String g_ct) {
+        this.g_ct = g_ct;
+    }
+
+    public String getG_fg() {
+        return g_fg;
+    }
+
+    public void setG_fg(String g_fg) {
+        this.g_fg = g_fg;
+    }
+
+    public String getG_gg() {
+        return g_gg;
+    }
+
+    public void setG_gg(String g_gg) {
+        this.g_gg = g_gg;
+    }
+
+    public String getG_mg() {
+        return g_mg;
+    }
+
+    public void setG_mg(String g_mg) {
+        this.g_mg = g_mg;
+    }
+
+    public String getG_ng() {
+        return g_ng;
+    }
+
+    public void setG_ng(String g_ng) {
+        this.g_ng = g_ng;
+    }
+
+    public String getG_p() {
+        return g_p;
+    }
+
+    public void setG_p(String g_p) {
+        this.g_p = g_p;
+    }
+
+    public String getG_t() {
+        return g_t;
+    }
+
+    public void setG_t(String g_t) {
+        this.g_t = g_t;
+    }
+
+}
